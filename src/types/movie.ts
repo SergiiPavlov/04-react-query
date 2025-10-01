c26ad4fd5081edc4446b0f26b79629544726b17e
Sergii Pavlov <pavlovsergii106@gmail.com>
2025-10-01 15:43:24 +0300

    chore(types): move TMDB response interface to types; rename to 04-react-query; bump to 0.4.0

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,9 +1,18 @@
+
+
 export interface Movie {
   id: number;
   poster_path: string | null;
-  backdrop_path: string | null;
   title: string;
   overview: string;
   release_date: string;
   vote_average: number;
+}
+
+
+export interface TMDBSearchResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
